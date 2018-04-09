4eb1c7dd32fb24cb618a447f04a75df9da3da9f6
Dan1elNeal <ishindanil96@gmail.com>
2018-04-10 01:05:56 +0500

    adding user to a dialog
    added patch router
    
    added user checking before adding
    
    check user before adding to dialog/contacts
    
    check user before adding to dialog/contacts

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -12,6 +12,12 @@ module.exports.add = async (req, res) => {
     };
 
     try {
+        await db.get(`users_${contact.username}`);
+    } catch (ex) {
+        return res.status(404).send(`User ${contact.username} not found`);
+    }
+
+    try {
         await db.post(`contacts_${req.user.id}`, JSON.stringify(contact));
     } catch (ex) {
         console.error(`Can't create contact ${contact}. Exception: ${ex}`);
