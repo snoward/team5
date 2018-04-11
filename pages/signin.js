0ad046b9a6159feaf1e1f2e3a7339b36312f0ea7
Mikhail Pliskovsky <snoward@users.noreply.github.com>
2018-04-12 00:03:40 +0500

    мимикрокодил

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SignIn from '../components/signin';
-const signInString = 'Войти';
+const signInString = 'Войти через Github';
 
 export default class SignInPage extends React.Component {
     render() {
