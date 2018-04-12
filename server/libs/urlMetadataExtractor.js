f60148d6ab07b670d973348be5bc2707da2e7f3a
Maxim Akulin <maxim.akulin@gmail.com>
2018-04-12 12:29:15 +0500

    enable eslint

--- server/libs/urlMetadataExtractor.js
+++ server/libs/urlMetadataExtractor.js
@@ -1,14 +1,17 @@
-/* eslint-disable */
 const urlMetadata = require('url-metadata');
+const urlRegExp = new RegExp('(https?:\\/\\/(?:www\\.|(?!www))' +
+    '[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]' +
+    '\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]' +
+    '\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]\\.[^\\s]{2,})');
 
 class urlMetadataExtractor {
     static async extract(url, options = null) {
         if (!options) {
             options = {
 
-            }
+            };
         }
-        
+
         return urlMetadata(url, options)
             .catch(() => null);
     }
@@ -16,16 +19,15 @@ class urlMetadataExtractor {
     static async extractFromText(text, options = null) {
         const url = this.getFirstUrl(text);
         if (!url) {
-            return null; 
+            return null;
         }
 
-        return this.extract(url, options)
+        return this.extract(url, options);
     }
 
     static getFirstUrl(text) {
-        const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/;
-        const matches = text.match(expression);
-        
+        const matches = text.match(urlRegExp);
+
         return matches ? matches[0] : null;
     }
 }
