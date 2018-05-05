9fb91ae2d10acbe624f4d83e4bd315f2054d18e2
Maxim Akulin <maxim.akulin@gmail.com>
2018-05-06 00:34:50 +0500

    invite link in conversation info

--- components/chat/ParticipantsModal/Participants/Participants.js
+++ components/chat/ParticipantsModal/Participants/Participants.js
@@ -14,15 +14,20 @@ export default class Participants extends React.Component {
     async updateParticipants(conversationId) {
         const conversation = await getConversationInfo(conversationId)
             .then(res => res.data);
-
+        // eslint-disable-next-line
+        const inviteLink = conversation.isPrivate ? null : `${window.location.origin}/join/${conversationId}`;
         this.setState({
-            participants: conversation.users
+            participants: conversation.users,
+            inviteLink
         });
     }
 
     render() {
         return (
             <div className='participants-container'>
+                {this.state.inviteLink &&
+                    <p className="invite-link">{this.state.inviteLink}</p>
+                }
                 <ol>
                     {this.state.participants.map((elem, idx) => {
                         return <div key={idx}>{elem}</div>;
